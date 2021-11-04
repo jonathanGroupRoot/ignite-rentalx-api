6b1bd871eac5f29a660c30e047e5c73b7bc58e29
Jonathan Vinicius Braz Silva <jonathangrouproot@gmail.com>
2021-11-04 11:54:48 -0300

    refactor: add config cors 🥇

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,9 +1,12 @@
 import 'reflect-metadata';
+
 import 'dotenv/config';
 import '@shared/container';
 
-import express, { NextFunction, Request, response, Response } from 'express';
+import cors from 'cors';
+import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
+
 import swaggerUi from 'swagger-ui-express';
 
 import upload from '@config/upload';
@@ -20,6 +23,7 @@ app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
+app.use(cors());
 app.use(router);
 
 app.get('/', (request, response) => {
